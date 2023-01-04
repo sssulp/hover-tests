eee9622470d9b0fa647f1df1627a731e0822bf89
Andre <andre.shotwell@gmail.com>
2023-01-04 07:37:57 -0500

    migrating local files to git

---
+++ script.js
@@ -0,0 +1,19 @@
+const subtitle = document.getElementsByClassName("subtitle")[0];
+
+const createWord = (text, index) => {
+  const word = document.createElement("span");
+  
+  word.innerHTML = `${text} `;
+  
+  word.classList.add("subtitle-word");
+  
+  word.style.transitionDelay = `${index * 40}ms`;
+  
+  return word;
+}
+
+const addWord = (text, index) => subtitle.appendChild(createWord(text, index));
+
+const createSubtitle = text => text.split(" ").map(addWord);
+
+createSubtitle("Hi, thanks for coming by - please leave a like, and subscribe");
